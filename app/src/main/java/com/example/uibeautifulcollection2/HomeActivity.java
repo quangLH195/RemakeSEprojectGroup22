e30915c9d5b8f43875a9b3e8c41ada3b65a7ce98
quangLH195 <59214933+quangLH195@users.noreply.github.com>
2020-04-16 18:15:30 +0700

    [Initial commit]

--- app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
+++ app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
-//    private
+//    private//see ?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
